f454a69af958de0a1448005b08e3897bbe95f26d
nelsonderen24 <33095123+nelsonderen24@users.noreply.github.com>
2017-11-20 00:07:02 +0100

    Correction finale

--- action.c
+++ action.c
@@ -10,6 +10,7 @@ void afficherOrdreCroissantTemps(Liste* liste){
     rythmeC = rythmeC->suivant;
   }
 }
+
 void afficherOrdreDecroissantTemps(Liste* liste){
   RythmeC* rythmeC = liste->dernier;
   while((rythmeC->precedent) != NULL){
@@ -33,18 +34,19 @@ void afficherTableauCreeCroissant(Liste* liste, int taille){
 void  affichageTriInsertionCroissant(int* p, int taille){
   int i, j;
   for (i = 2; i < (taille*2) ; i = i+2) {
-       int elemPouls = p[i];
-       int elemTemps = p[i+1];
-       for (j = i; j > 0 && p[j-2] > elemPouls; j = j-2){
-           p[j] = p[j-2];
-           p[j+1] = p[j-1];
+    int elemPouls = p[i];
+    int elemTemps = p[i+1];
+    for (j = i; j > 0 && p[j-2] > elemPouls; j = j-2){
+      p[j] = p[j-2];
+      p[j+1] = p[j-1];
    }
-   p[j] = elemPouls;
-   p[j+1] = elemTemps;
+    p[j] = elemPouls;
+    p[j+1] = elemTemps;
   }
-  for(int i = 0; i < taille*2; i = i+2) printf("%d / %d\n ", p[i], p[i+1]);
+  for(i = 0; i < taille*2; i = i+2){
+    printf("%d / %d\n ", p[i], p[i+1]);
+  }
 }
-
 void afficherTableauCreeDecroissant(Liste* liste, int taille){
   int tab[tailleTableau];
   RythmeC* rythmeC = liste->premier;
@@ -60,16 +62,16 @@ void afficherTableauCreeDecroissant(Liste* liste, int taille){
 void affichageTriInsertiondDecroissant(int* p, int taille){
   int i, j;
   for (i = 2; i < (taille*2) ; i = i+2) {
-       int elemPouls = p[i];
-       int elemTemps = p[i+1];
-       for (j = i; j > 0 && p[j-2] > elemPouls; j = j-2){
-           p[j] = p[j-2];
-           p[j+1] = p[j-1];
+    int elemPouls = p[i];
+    int elemTemps = p[i+1];
+    for (j = i; j > 0 && p[j-2] > elemPouls; j = j-2){
+      p[j] = p[j-2];
+      p[j+1] = p[j-1];
    }
-   p[j] = elemPouls;
-   p[j+1] = elemTemps;
+    p[j] = elemPouls;
+    p[j+1] = elemTemps;
   }
-  for(int i = (taille*2)-1; i > 0; i = i-2) printf("%d / %d\n", p[i-1], p[i]);
+  for(i = (taille*2)-1; i > 0; i = i-2) printf("%d / %d\n", p[i-1], p[i]);
 }
 
 void afficherTempsChoisi(Liste* liste,int tempsDebut,int tempsFin) {
@@ -77,30 +79,32 @@ void afficherTempsChoisi(Liste* liste,int tempsDebut,int tempsFin) {
     int pouls = 0;
     int temps = 0;
     while (rythmeC->temps <= tempsFin && rythmeC->suivant != NULL) {
-        if (rythmeC->temps >= tempsDebut) {
-            pouls = rythmeC->pouls;
-            temps = rythmeC->temps;
-            printf("%d ; %d\n", temps, pouls);
-        }
-        rythmeC= rythmeC->suivant;
+      if (rythmeC->temps >= tempsDebut) {
+        pouls = rythmeC->pouls;
+        temps = rythmeC->temps;
+        printf("%d ; %d\n", temps, pouls);
+      }
+      rythmeC= rythmeC->suivant;
     }
 }
+
 void afficherMoyenneTempsChoisi(Liste* liste,int tempsDebut,int tempsFin) {
     RythmeC* rythmeC= liste->premier;
     int pouls = 0;
     int x = 0;
     int y = 0;
     while (rythmeC->temps <= tempsFin && rythmeC->suivant != NULL) {
-        if (rythmeC->temps >= tempsDebut) {
-            pouls = rythmeC->pouls;
-            x = x + pouls;
-            y++;
-        }
-        rythmeC= rythmeC->suivant;
+      if (rythmeC->temps >= tempsDebut) {
+        pouls = rythmeC->pouls;
+        x = x + pouls;
+        y++;
+      }
+      rythmeC= rythmeC->suivant;
     }
     float moyenne = x/y;
     printf("La moyenne du pouls dans la plage de temps donnee est de : %f", moyenne);
 }
+
 void afficherNombresDeDonneesStockees(Liste* liste){
     int compteur = calculerNombresDeDonneesStockees(liste);
     printf("Il y %d lignes de donnees actuellement en memoire", compteur);
