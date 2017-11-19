0b916c708be7810be6eb3a473c055d244679ad8b
nelsonderen24 <33095123+nelsonderen24@users.noreply.github.com>
2017-11-19 13:31:36 +0100

    prototype action.c

--- action.c
+++ action.c
@@ -1,3 +1,25 @@
 #include "menu.h"
 #include "donnee.h"
 #include "action.h"
+
+afficherMaxMin(Liste* liste) {
+  RythmeC* rythmeC = liste->premier;
+  int poulsMax = rythmeC->pouls;
+  int tempsMax = rythmeC->temps;
+  int tempsMin = rythmeC->temps;
+  int poulsMin = rythmeC->pouls;
+  while (rythmeC->suivant != NULL) {
+    rythmeC = rythmeC->suivant;
+    if (rythmeC->pouls > poulsMax) {
+      poulsMax = rythmeC->pouls;
+      tempsMax = rythmeC->temps;
+    }
+    if (rythmeC->pouls < poulsMax) {
+      poulsMin = rythmeC->pouls;
+      tempsMin = rythmeC->temps;
+    }
+  }
+  printf("Valeur du poulsMax/temps : %d / %d\nValeur du poulsMin/temps : %d / %d\n", poulsMax, tempsMax, poulsMin, tempsMin);
+
+
+}
